e06cf158df52d18cc26da72653afa37a1c1afa8c
Nate Hefner <natehefner@Nathans-Air.fios-router.home>
2016-10-02 15:38:24 -0400

    added javadoc for MontyHallGame

--- MontyHall/MontyHallGame.java
+++ MontyHall/MontyHallGame.java
@@ -1,14 +1,33 @@
+/**
+*This is the Monty Hall game using a user input
+*There are 3 doors, two with goats, one with a car
+*The user picks a door, then one door with a goat is revealed,
+*and then the user is asked if they want to switch
+*Then, the program tells the user if the final door they chose has a goat or a car
+*@author Nate Hefner
+*@version 1.0
+*
+*/
 import java.util.Scanner;
 
 public class MontyHallGame
 {
+	/**
+	*This is the main method that the java interpreter calls
+	*It calls the two functions needed to run the program
+	*
+	*/
 	public static void main (String[] args)
 	{
 		welcome();
 		
 		game();
 	}
-	
+	/**
+	*This is the function that executes the welcome format for the user.
+	*It informs the reader how to play the game
+	*
+	*/
 	public static void welcome()
 	{
 		System.out.println("This is the Monty Hall Game");
@@ -16,7 +35,14 @@ public class MontyHallGame
 		System.out.println("You will have a chance to switch ");
 		System.out.println("after a door has been revealed. ");
 	}
-	
+	/**
+	*This is the function executes all of the gameplay mode for the user
+	*It uses the scanner to interact with the user
+	*It also uses doorToReveal() function to reveal a door that doesn't have a car
+	*and isn't the userPick
+	*Tells the user if they won a car or a goat
+	*
+	*/
 	public static void game()
 	{
 		Scanner keyboard = new Scanner(System.in);
@@ -55,7 +81,7 @@ public class MontyHallGame
 	}
 	/**
 	*Purpose of function: To Reveal a door that was not chosen by the user and that does not have a car
-	*
+	*This function also checks to see if the final door the user chose is a goat or a car
 	*Algorithm: With while loop, checks each door to see if it is the user's choice OR has a car, 
 	*hence variables c (car) and u (user choice)
 	*if i is equal to either of these, it stays in the loop
@@ -70,20 +96,5 @@ public class MontyHallGame
 		}
 		return i;
 	}
-	
-	/**
-	*Purpose of function: 
-	*
-	*
-	*/
-	
-	public static int goatReveal1(int c, int u)
-	{
-		for(int i = 1; i < 4; i++)
-		{
-			if (i !=c && i !=u)
-				return i;
-		}
-		return -1;
-	}
+
 }
